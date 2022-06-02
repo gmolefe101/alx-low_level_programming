994cf8c28be6704692bb776f381d5380bc6baf0a
gmolefe101 <mrgmolefe101@gmail.com>
2022-06-01 23:36:54 -0700

    Puts intial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#includes <stdio.h>
+/**
+ * main - Entry Point
+ *  
+ * Return: always 0
+ *
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
