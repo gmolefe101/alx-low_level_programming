74f0a07c7c1a81b00467603e62198c645b6dec27
gmolefe101 <mrgmolefe101@gmail.com>
2022-06-13 13:49:54 -0700

    working files commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* Reset_to_98 - write a function that takes a pointer to an int as parameter
+* and updates the value it points to to 98
+* @n: input
+*
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MIAN_H
+void reset_to_98(int *n);
+
+
+#endif /* MAIN_H */
