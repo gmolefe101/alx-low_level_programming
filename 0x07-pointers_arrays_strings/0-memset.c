1a42eb2e6f8178178614ce945c85fcbdabf9ca16
gontsemolefe <john.konene@gmail.com>
2022-06-22 02:23:26 +0200

    Betty error messaging

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * @s: simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
- * Return: s
+ * Return: nothing
  */
 char *_memset(char *s, char b, unsigned int n)
 {
