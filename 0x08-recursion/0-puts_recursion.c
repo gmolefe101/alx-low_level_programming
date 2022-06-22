b923ce01ea4daa0b5c0c71b3e9437e667c0531a5
gmolefe101 <mrgmolefe101@gmail.com>
2022-06-22 16:29:06 -0700

    betty error correction on task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
+
 /**
- * _puts_recursion
- * @s: string to recusion
- *
- * Return: Always 0.
+ * _puts_recursion - Print the given string w/ _putchar recursively
+ * @s: The given string
+ * Return: 0 if succesful exit
  */
 void _puts_recursion(char *s)
 {
-    if(*s)
-    {
-        _putchar(*s);
-        _puts_recursion(++s);
-    }
-    else
-        _putchar('\n');
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+	else
+		_putchar('\n');
 }
