f5977e437687537673c42af8a83e99e2f4ca374a
gmolefe101 <mrgmolefe101@gmail.com>
2022-06-22 17:01:41 -0700

    task 0 quick fix

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursion(s + 1);
+		_puts_recursion(++s);
 	}
-	else if (*s == '\0')
+	else
 	{
 		_putchar('\n');
 	}
