7bf6523c909cb499cd997521758b3572e48c365b
gmolefe101 <mrgmolefe101@gmail.com>
2022-06-23 23:35:57 -0700

    task one initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - main function
+* @argc: Argument count
+* @argv: Vertor of arguments
+*Return: Always 0
+*/
+int main(int argc, char *agrv[])
+{
+	printf("%s", argv[0];
+	return (0);
+}
